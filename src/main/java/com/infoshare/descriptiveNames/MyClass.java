aa5d683f73ed17fd4fa2278bd2125fd4a271893b
karol <ka.lipiec@gmail.com>
2019-02-13 17:34:59 +0100

    max

--- src/main/java/com/infoshare/descriptiveNames/MyClass.java
+++ src/main/java/com/infoshare/descriptiveNames/MyClass.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 public class MyClass {
 
-    public int c(List<Integer> l) {
+    public int c(List<Integer> l, int g) {
         int s = 0;
         for (int i = 0; i < l.size(); i++) {
-            if (l.get(i) > 3 && l.get(i) % 2 == 0) {
+            if (l.get(i) > g && l.get(i) % 2 == 0) {
                 s = s + l.get(i) * l.get(i);
             }
         }
